f01ed248fe3ca91f12e62103606a6ee6ed88031f
Den1sproger <114018798+Den1sproger@users.noreply.github.com>
2023-10-04 16:12:26 +0300

    feat: add launch file

---
+++ app.py
@@ -0,0 +1,21 @@
+import logging
+
+from aiogram import executor
+from telegram_bot import dp, set_default_commands
+# from telegram_bot.handlers.inline_buttons import *
+from telegram_bot.handlers.start import *
+from telegram_bot.handlers.default_commands import *
+
+
+# LOG_FILENAME = "/home/statistics_management/py_log.log"
+# logging.basicConfig(level=logging.INFO, filename=LOG_FILENAME, filemode="w")
+
+
+async def on_startup(_):
+    await set_default_commands(dp)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp,
+                           skip_updates=True,
+                           on_startup=on_startup)
